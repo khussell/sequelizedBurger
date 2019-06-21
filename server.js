7e308d78d2ab3595e15bb077e797ee6df536f8ed
Lilu <KHussell@gmail.com>
2019-06-21 13:38:56 -0400

    files created and server.js done

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const PORT = process.env.PORT || 3030;
+
+const app = express()
+
+var db = require("./models")
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static directory
+app.use(express.static("public"));
+
+// Routes
+// =============================================================
+require("./routes/html-routes.js")(app);
+require("./routes/author-api-routes.js")(app);
+require("./routes/post-api-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
