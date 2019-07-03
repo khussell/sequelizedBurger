764b9bdd3d89c5bd7153a290b667004e22b5134b
Lilu <KHussell@gmail.com>
2019-07-03 19:41:26 -0400

    schema created for database

---
+++ schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS sequelized_burger_db;
+
+CREATE DATABASE sequelized_burger_db;
--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(express.static("public"));
 // Routes
 // =============================================================
 require("./routes/html-routes.js")(app);
-require("./routes/author-api-routes.js")(app);
-require("./routes/post-api-routes.js")(app);
+require("./routes/burger-api-routes.js")(app);
+require("./routes/eater-api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
