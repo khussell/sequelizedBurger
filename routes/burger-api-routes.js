257784467d8706e3f2d9ab9e7f32599318b450e7
Lilu <KHussell@gmail.com>
2019-07-03 21:25:19 -0400

    index.handlebars now gets info from / route

--- routes/burger-api-routes.js
+++ routes/burger-api-routes.js
@@ -1,6 +1,19 @@
 var db = require("../models");
 
 module.exports = function (app) {
+  app.get("/", function(req,res){
+      db.Burger.findAll({}).then(function(data){
+          console.log(data)
+          res.render("index", {burgers: data})
+      })
+  })
+
+
+   app.get("/api/burgers", function(req,res){
+       db.Burger.findAll({}).then(function(dbBurgers){
+           res.json(dbBurgers)
+       })
+   })
 
     app.post("/api/burgers", function (req, res) {
         var newBurger = req.body
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,8 +3,8 @@ var path = require("path");
 
 module.exports = function(app){
 
-    app.get("/", function(req, res) {
-        res.render("index");
-      });
+   // app.get("/", function(req, res) {
+    //    res.render("index");
+    //  });
 
     }
